360eff0e32eb2b18df950cb5e260d92ebbded13a
roohankhan <roohankhan@byklabs.com>
2026-02-17 17:29:26 +0500

    migration files created

---
+++ database/migrations/2026_02_17_115301_folders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('folders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            // null if root folder
+            $table->foreignId('parent_id')->nullable()->constrained('folders')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('folders', function (Blueprint $table) {
+            $table->index('user_id');
+            $table->index('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('folders');
+    }
+};
